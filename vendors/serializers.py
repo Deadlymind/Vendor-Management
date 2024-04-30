3730c55d6d0720101b13acee619ea04d022cd1b7
Deadlymind <deadlygamex5@gmail.com>
2024-04-30 15:44:21 +0100

    switch api from FBV to CBV

--- vendors/serializers.py
+++ vendors/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Vendor, PurchaseOrder
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
 
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,9 @@ class VendorSerializer(serializers.ModelSerializer):
 class PurchaseOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = PurchaseOrder
+        fields = '__all__'
+
+class HistoricalPerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HistoricalPerformance
         fields = '__all__'
